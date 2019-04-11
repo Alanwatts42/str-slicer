5a707ca7583668d30c0b7f35abce4e48dd0e3cc2
Alanwatts42 <evanzemail@gmail.com>
2019-04-11 07:22:35 -0500

    updating 11apr19 pushing to remote

--- slice.py
+++ slice.py
@@ -1,21 +1,26 @@
-
 # -*- coding: UTF-8 -*-
+import os
+import os.path
 
-# For testing techniques for text parsing without building ui
+"""For testing techniques for text parsing without building ui"""
 
-def terminaltext(args):
-    infile = open(data/aptlist.txt)
-    outfile = open(data/output.csv, 'w')
+def getpath(dir_lo = '.'):
+    cp = os.getcwd()
+    acp = os.path.abspath(cp)
+    output acp.split(os.path.sep)
+
+def grabfile(args):
+    infile = open("data/aptlist.txt")
+
+   outfile = open("data/output.csv", 'w')
 
     for i in range(1):
         infile.next() # skip first line
     for line in infile:
         outfile.writelines(lines, reps)
 
+def writefile():
+    pass
 
-         reps = {          '"NAN"': 'NAN',          '"': '',          '0-':
-                 '0,',          '1-': '1,',          '2-': '2,',          '3-':
-                 '3,',          '4-': '4,',          '5-': '5,',          '6-':
-                 '6,',          '7-': '7,',          '8-': '8,',          '9-':
-                 '9,',          ' ': ',',          ':': ','  
-                }
+if __name__ == '__main__':
+    print(getpath())

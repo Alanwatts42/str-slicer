c122f952fa0123cf5f93a19cda413e036745536f
Alanwatts42 <evanzemail@gmail.com>
2019-09-08 18:07:42 -0500

    rearranged project structure

---
+++ str-slicer-filetree.sh
@@ -0,0 +1,29 @@
+[01;34m.[00m
+├── LICENSE
+├── README.md
+├── [01;34mstring-slicer[00m
+│   ├── [01;32mapp.py[00m
+│   ├── [01;34mconfig[00m
+│   │   ├── __init__.py
+│   │   ├── parse.py
+│   │   ├── read.py
+│   │   ├── slice.py
+│   │   └── write.py
+│   ├── __init__.py
+│   ├── [01;34minterface[00m
+│   │   ├── __init__.py
+│   │   └── slice_ui.py
+│   ├── __main__.py
+│   ├── [01;34mresources[00m
+│   │   ├── NOTES.md
+│   │   └── python-text_parsing_guide.pdf
+│   ├── [01;34mslicers[00m
+│   │   ├── __init__.py
+│   │   ├── [01;32mlog_splitter.py[00m
+│   │   ├── parser-frame_2.py
+│   │   └── [01;32mparser-frame.py[00m
+│   └── [01;34mtests[00m
+│       └── test.dat
+└── str-slicer-filetree.txt
+
+6 directories, 20 files
---
+++ str-slicer/tests/list-slicer_test.py
@@ -0,0 +1,4 @@
+#!/usr/bin/env python3
+from re import findall
+from os import sys
+

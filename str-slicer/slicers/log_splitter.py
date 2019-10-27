5134e65a3fc1f9e4b8f4ab2b31d0c4546c716a64
Alanwatts42 <evanzemail@gmail.com>
2019-10-27 01:28:07 -0500

    progress on log_splitter nothing yet on str-slicer.py main file

--- str-slicer/slicers/log_splitter.py
+++ str-slicer/slicers/log_splitter.py
@@ -1,13 +1,30 @@
 import re
+import os
+import os.path as path
 
-syslog = '/var/log/syslog'  # log to be searched
-sys_errors = []
-line_num = 0
-pattern = re.compile(".*error.*", re.IGNORECASE)  # regex search pattern
-with open(syslog, 'rt') as logfile:
-    for entry in logfile:
-        line_num += 1
-        if pattern.search(entry) is not None:
-            sys_errors.append((line_num, entry.rstrip('\n')))
-for error in sys_errors:
-    print("Line " + str(error[0]) + ": " + error[1])
+tgt = input("path of logfile:")  # log to be searched
+# error = input("enter error to search for:")
+
+def error_search(srchlog):
+    
+    results = []
+    line_num = 0
+    
+    pattern = re.compile(".*error.*", re.IGNORECASE)
+    
+    with open(srchlog, 'rt') as logfile:
+        for entry in logfile:
+            line_num += 1
+            if pattern.search(entry) is not None:
+                results.append((line_num, entry.rstrip('\n')))
+
+    for result in results:
+        print("Line " + str(result[0]) + ": " + result[1])
+
+    
+
+
+if __name__ == '__main__':
+    
+    if path.isfile(tgt) is True:
+        error_search(tgt)
